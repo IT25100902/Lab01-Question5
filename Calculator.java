166ed21744cfd1abedf2f4a140b1fb83c0f260c6
IT25100902 <IT25100902@my.sliit.lk>
2026-01-28 15:47:17 +0530

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -16,3 +16,31 @@ public class Calculator {
     }
 }
 
+ class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+       
+        int part1 = calc.multiply(3, 4);
+        int part2 = calc.multiply(5, 7);
+        int sum1 = calc.add(part1, part2);
+        int result1 = calc.square(sum1);
+
+        
+        int sum2a = calc.add(4, 7);
+        int square2a = calc.square(sum2a);
+
+        int sum2b = calc.add(8, 3);
+        int square2b = calc.square(sum2b);
+
+        int result2 = calc.add(square2a, square2b);
+
+        System.out.println("Result 1 = " + result1);
+        System.out.println("Result 2 = " + result2);
+    }
+}
+
+
+
